b6ef4c7c2f9c9d2deb2a0ab02518aad7c7a9f279
PhuongNguyen61 <phuongnguyen.612003@gmail.com>
2025-03-04 20:49:14 +0700

    fix

--- Fe/src/components/postingCarInfoPage/index.jsx
+++ Fe/src/components/postingCarInfoPage/index.jsx
@@ -21,18 +21,12 @@ const listBrand = [
   "Mazda",
   "Ford",
   "Subaru",
-  "Hyundai",
   "Mercedes",
   "Audi",
   "BMW",
 ];
 
 const listYear = [
-  "2010",
-  "2011",
-  "2012",
-  "2013",
-  "2014",
   "2015",
   "2016",
   "2017",
